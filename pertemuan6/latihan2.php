6ec6ad2af3c91aafa7c96fab9761453927ba5fa1
Razn1 <itoushura12@gmail.com>
2025-02-15 23:47:24 +0700

    Upload Pertemuan 6

---
+++ pertemuan6/latihan2.php
@@ -0,0 +1,48 @@
+<?php 
+// $mahasiswa = [
+//     ["Faisa Al Farrel", "0110224149","alfarrel218@gmail.com", "Teknik Informatika"],
+//     ["Saya", "0110321321", "saya@gmail.com", "Teknik Informatika"],
+//     ["Kamu", "01103213221", "kamu@gmail.com", "Teknik Informatika"],
+// ];
+
+// Array Asosiatif
+// Definisinya sama seperti array numerik, namun
+// Key-nya adalah string yang kita buat sendiri
+
+$mahasiswa = [
+    [
+    "nama" => "Faisa Al Farrel",
+    "nim" => "0110224149",
+    "email" => "alfarrel218@gmail.com", 
+    "jurusan" => "Teknik Informatika"
+    ],
+    [
+        "nama" => "Al Farrel",
+        "nim" => "0110999999",
+        "email" => "alfarrel99@gmail.com", 
+        "jurusan" => "Teknik Informatika"
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <?php foreach ($mahasiswa as $mhs) : ?>
+        <ul>
+            <li>Nama : <?= $mhs["nama"]; ?></li>
+            <li>Nim : <?= $mhs["nim"]; ?></li>
+            <li>Jurusan : <?= $mhs["jurusan"]; ?></li>
+            <li>Email : <?= $mhs["email"]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+</html>
+
